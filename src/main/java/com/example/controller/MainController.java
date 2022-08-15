5ce6c2144e91c8455fc9ffb9eb2d9d209b94a54a
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-08-15 03:04:16 -0400

    Update MainController.java

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -5,14 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	@RequestMapping("/")
-	    String home() {
-		return "Hello World!";
-	 }
+
 	@GetMapping("/person")
 	public @ResponseBody ResponseEntity<String> getWord(){
 		return new ResponseEntity<>("Happy Thursday, yo", HttpStatus.OK);
