5947e47b9731b339628607b7c18dc4d421f8997c
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-08-15 03:06:07 -0400

    Update MainDriver.java

--- src/main/java/com/example/MainDriver.java
+++ src/main/java/com/example/MainDriver.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 /*
  * @Configuration - mark the class as a source for bean definitions
  * for the application context
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
  * @ComponentScan tells Spring to look for other components,
  * configurations, services in other packages
  */
-
+@RestController
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.example*"})
-@CrossOrigin(origins = "http://localhost:4200")
 public class MainDriver {
-	
+	@RequestMapping("/")
+	    String home() {
+		return "Hello World!";
+	 }
 	public static void main(String[] args) {
 		SpringApplication.run(MainDriver.class, args);
 	}
