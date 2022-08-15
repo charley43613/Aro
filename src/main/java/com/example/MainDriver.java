5972c17736502347b2cd72272e70810c476f30d1
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-08-15 09:50:55 -0400

    Update MainDriver.java

--- src/main/java/com/example/MainDriver.java
+++ src/main/java/com/example/MainDriver.java
@@ -3,13 +3,8 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 /*
  * @Configuration - mark the class as a source for bean definitions
  * for the application context
@@ -23,12 +18,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainDriver {
 	@RequestMapping("/")
 	    String home() {
-		return "Hello World!";
+		return "Hello Redhat Openshift!";
 	 }
-	@GetMapping("/welcome")
-	public @ResponseBody ResponseEntity<String> getWord(){
-		return new ResponseEntity<>("Welcome to redhat Openshift", HttpStatus.OK);
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(MainDriver.class, args);
 	}
